0f3b59e0cfd44beede6caecffdef6ac58458575d
SeYunJung <77439732+SeYunJung@users.noreply.github.com>
2025-09-03 12:54:51 +0900

    [Fix] 점프했을 때 방향 전환되도록 수정

--- Assets/2. Scripts/Manager/CharacterManager.cs
+++ Assets/2. Scripts/Manager/CharacterManager.cs
@@ -7,4 +7,8 @@ public class CharacterManager : Singleton<CharacterManager>
     // Player, 몬스터 등 캐릭터 관리
     public Boss _boss;
     public Boss Boss { get { return _boss; } set { _boss = value; } }
+
+    // Player
+    public PlayerStat _playerStat;
+    public PlayerStat PlayerStat { get { return _playerStat; } set { _playerStat = value; } }
 }
--- Assets/2. Scripts/Player/State/PlayerRunState.cs
+++ Assets/2. Scripts/Player/State/PlayerRunState.cs
@@ -4,30 +4,55 @@ public class PlayerRunState : BaseState
 {
     private PlayerController playerController;
 
-    public override void EnterState(StateManager stateManager)
+    public override void EnterState(StateMachine stateMachine)
     {
         Debug.Log("Hello from the Run State");
-        this.playerController = stateManager.PlayerController;
+        this.playerController = stateMachine.PlayerController;
         ChangeSpeed();
     }
 
-    public override void UpdateState(StateManager stateManager)
+    public override void UpdateState(StateMachine stateMachine)
     {
 
     }
 
-    public override void OnCollisionEnter(StateManager stateManager, Collision2D collision)
+    public override void OnCollisionEnter(StateMachine stateMachine, Collision2D collision)
     {
 
     }
 
-    public override void FixedUpdateState(StateManager stateManager)
+    public override void OnTriggerEnter(StateMachine stateMachine, Collision2D collision)
     {
 
+    }
+
+    public override void FixedUpdateState(StateMachine stateMachine)
+    {
+        //Debug.Log("(FixedUpdateState): Player Run!!");
+        Run();
     }
 
     private void ChangeSpeed()
     {
-        this.playerController.SpeedModifier = playerController.SpeedModifierInput;
+        CharacterManager.Instance.PlayerStat.SpeedModifier = CharacterManager.Instance.PlayerStat.SpeedModifierInput;
+        //this.playerController.SpeedModifier = playerController.SpeedModifierInput;
+    }
+
+    private void Run()
+    {
+        // 이동 방향 설정
+        playerController.MovementDirection = playerController.MovementInput;
+
+        // 이동 속도 설정
+        playerController.MovementDirection *= (CharacterManager.Instance.PlayerStat.MoveSpeed * CharacterManager.Instance.PlayerStat.SpeedModifier);
+
+        // 중력은 velocity.y값으로 설정
+        Vector2 dir = playerController.MovementDirection;
+        dir.y = playerController.Rigid.velocity.y;
+        playerController.MovementDirection = dir;
+        //playerController.MovementDirection.y = playerController.Rigid.velocity.y;
+
+        // 이동 처리
+        playerController.Rigid.velocity = playerController.MovementDirection;
     }
 }
