6e8c9abffb4d7a30e28c7be62f40761407cf9ff0
LeeYoungSin <dldudtls100@gmail.com>
2025-09-04 21:14:34 +0900

    [Feat] 보스룸 카메라 애니메이션 추가

---
+++ Assets/2. Scripts/Camera/OnTriggerCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerCamera : MonoBehaviour
+{
+    void SetisMove()
+    {
+        /*if(CharacterManager.instance.PlayerStat.isMoved == false)
+        {
+            CharacterManager.instance.PlayerStat.isMoved = true;
+        }
+        else
+        {
+            CharacterManager.instance.PlayerStat.isMoved = false;
+        }*/
+        Debug.Log("카메라 이벤트 출력확인용");
+    }
+}
