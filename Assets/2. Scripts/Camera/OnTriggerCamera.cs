80ca221fcd1c370eea8afec27cd7bd40b78278ee
LeeYoungSin <dldudtls100@gmail.com>
2025-09-04 23:01:53 +0900

    [Feat] 보스 효과음 설정

--- Assets/2. Scripts/Camera/OnTriggerCamera.cs
+++ Assets/2. Scripts/Camera/OnTriggerCamera.cs
@@ -20,14 +20,19 @@ public class OnTriggerCamera : MonoBehaviour
 
     void SetisMove()
     {
-        /*if(CharacterManager.instance.PlayerStat.isMoved == false)
+        if(CharacterManager.instance.PlayerStat.isMoved == false)
         {
             CharacterManager.instance.PlayerStat.isMoved = true;
         }
         else
         {
             CharacterManager.instance.PlayerStat.isMoved = false;
-        }*/
+        }
         Debug.Log("카메라 이벤트 출력확인용");
+    }
+
+    void SetBossEFX()
+    {
+        SoundManager.Instance.PlayEFXSound("BossAppear_EFX");
     }
 }
